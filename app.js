2f211ac35ceabc2d7b59f64f3b13deab37865693
chadwithuhc <chadwithuhc@gmail.com>
2017-10-11 13:47:53 -0600

    CORS

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const favicon = require('serve-favicon')
 const logger = require('morgan')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
+const cors = require('cors')
 
 // DB CONNECTION
 const knex = require('./db/knex')
@@ -22,6 +23,7 @@ const app = express()
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
 app.use(logger('dev'))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
